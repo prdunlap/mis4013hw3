93328f330db4fc199c564a804157af35891f7823
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-09-23 20:07:43 -0500

    Create utility-db.php

---
+++ utility-db.php
@@ -0,0 +1,12 @@
+<?php
+function get_db_connection(){
+    // Create connection
+    $conn = new mysqli('129.15.65.247', 'PrestonDunlap', 'Memorial21!', 'mis4013-db');
+    
+    // Check connection
+    if ($conn->connect_error) {
+      return false;
+    }
+    return $conn;
+}
+?>
