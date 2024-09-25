e056086da9277d2eef1cde56c23a50a3d1d52c00
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-09-25 13:45:10 -0500

    Update utility-db.php

--- utility-db.php
+++ utility-db.php
@@ -4,7 +4,6 @@ function get_db_connection(){
     $conn = new mysqli('129.15.65.247', 'PrestonDunlap', 'Memorial21!', 'mis4013-db');
 
 
-    
     // Check connection
     if ($conn->connect_error) {
       return false;
