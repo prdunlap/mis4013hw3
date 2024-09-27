62117c1d41c7538b68954b03a3dcfcf9b0869050
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-09-27 11:26:14 -0500

    Update loan.php

--- loan.php
+++ loan.php
@@ -1,7 +1,5 @@
-<? php
-require_once("utility-db.php");
+<?php require_once("utility-db.php");
 require_once("model-loans.php");
-
 $pageTitle = "Loans";
 include "view-header.php";
 $loans = selectLoans();
