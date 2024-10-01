e2d6107f7e11ae2f68de2eb0e439c9da98ba9afb
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-01 15:43:15 -0500

    Update view-borrowers.php

--- view-borrowers.php
+++ view-borrowers.php
@@ -17,11 +17,11 @@ while ($borrower = $borrowers->fetch_assoc()) {
 ?> 
   <tr>
     <td><?php echo $borrower['BorrowerID']; ?></td>
-    <td><?php echo $author['FName']; ?></td>
-    <td><?php echo $author['LName']; ?></td>
-    <td><?php echo $author['DOB']; ?></td>
-    <td><?php echo $author['email']; ?></td>
-    <td><?php echo $author['phone']; ?></td>
+    <td><?php echo $borrower['FName']; ?></td>
+    <td><?php echo $borrower['LName']; ?></td>
+    <td><?php echo $borrower['DOB']; ?></td>
+    <td><?php echo $borrower['email']; ?></td>
+    <td><?php echo $borrower['phone']; ?></td>
   </tr>
 <?php 
 }
