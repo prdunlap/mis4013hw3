357044781ffbd1bf0861489c314db023953b2da3
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-02 13:33:26 -0500

    Update model-loans-by-borrower.php

--- model-loans-by-borrower.php
+++ model-loans-by-borrower.php
@@ -1,6 +1,6 @@
 <?php
 function selectLoansByBorrower($lid) {
-    try {
+
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT loan.LID, loan.BorrowerID, loan.CheckoutDate, loan.DueDate, borrower.FName, borrower.LName FROM loan JOIN borrower ON loan.BorrowerID = borrower.BorrowerID");
          $stmt->bind_param("i", $iid);
@@ -9,11 +9,4 @@ function selectLoansByBorrower($lid) {
         $conn->close();
         
         return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-
 ?>
