d6a3ad3a82e18db7b4ca32ed51c04ff963d1bf0d
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-02 14:11:33 -0500

    Update view-loans-by-borrower.php

--- view-loans-by-borrower.php
+++ view-loans-by-borrower.php
@@ -13,15 +13,15 @@
     </thead>
     <tbody>  
 <?php
-while (loanby = $loansby->fetch_assoc()) {
+while (loanby=$loansby->fetch_assoc()) {
 ?> 
   <tr>
-    <td><?php echo $loanby['LID']; ?></td>
-    <td><?php echo $loanby['BorrowerID']; ?></td>
-    <td><?php echo $loanby['CheckoutDate']; ?></td>
-    <td><?php echo $loanby['DueDate']; ?></td>
-    <td><?php echo $loanby['FName']; ?></td>
-    <td><?php echo $loanby['LName']; ?></td>
+    <td><?php echo $loansby['LID']; ?></td>
+    <td><?php echo $loansby['BorrowerID']; ?></td>
+    <td><?php echo $loansby['CheckoutDate']; ?></td>
+    <td><?php echo $loansby['DueDate']; ?></td>
+    <td><?php echo $loansby['FName']; ?></td>
+    <td><?php echo $loansby['LName']; ?></td>
   </tr>
 <?php 
 }
