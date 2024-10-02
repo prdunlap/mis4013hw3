60516377eddc60b82a38b14794a82d60a905d115
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-02 11:11:11 -0500

    Update view-borrowers.php

--- view-borrowers.php
+++ view-borrowers.php
@@ -16,12 +16,12 @@
 while ($borrower = $borrowers->fetch_assoc()) {
 ?> 
   <tr>
-    <td><?php echo $borrower['BorrowerID']; ?></td>
-    <td><?php echo $borrower['FName']; ?></td>
-    <td><?php echo $borrower['LName']; ?></td>
-    <td><?php echo $borrower['DOB']; ?></td>
-    <td><?php echo $borrower['Email']; ?></td>
-    <td><?php echo $borrower['Phone #']; ?></td>
+    <td><?php echo $borrower[BorrowerID]; ?></td>
+    <td><?php echo $borrower[FName]; ?></td>
+    <td><?php echo $borrower[LName]; ?></td>
+    <td><?php echo $borrower[DOB]; ?></td>
+    <td><?php echo $borrower[Email]; ?></td>
+    <td><?php echo $borrower[Phone Number]; ?></td>
     <td>
         <form method="post" action="loans-by-borrower.php">
           <input type ="hidden" name="lid" value="<?php echo $loan[loan_lid]; ?>">
