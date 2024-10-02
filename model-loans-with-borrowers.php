d845c6da7a240ca00f8c80855bc076364507e5c7
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-01 21:57:38 -0500

    Create model-loans-with-borrowers.php

---
+++ model-loans-with-borrowers.php
@@ -0,0 +1,18 @@
+<?php
+function selectLoans() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT LID, BorrowerID, CheckoutDate, DueDate FROM 'loan'");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+?>
