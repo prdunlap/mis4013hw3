43aee2c67671cf743e046d36066987694c315070
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-01 22:12:34 -0500

    Create view-loans-with-borrowers.php

---
+++ view-loans-with-borrowers.php
@@ -0,0 +1,29 @@
+<h1>Loans with Borrowers</h1>
+<div class="card-group">
+<?php
+$sql = "SELECT LID, BorrowerID, CheckoutDate, DueDate FROM loans";
+include "model-loans.php";
+$loans = $conn->query($sql);
+while ($loan = $loans->fetch_assoc()) {
+?> 
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $loan['BorrowerID']; ?></h5>
+      <p class="card-text">
+  <?php 
+    $loans = selectLoansByBorrower($loan['LID']);
+  while ($loan = $loans->fetch_assoc()) {
+    ?>
+
+        
+<?php 
+  }
+  ?>
+      
+      </p>
+      <p class="card-text"><small class="text-body-secondary"><?php echo $loan['BorrowerID']; ?></small></p>
+    </div>
+<?php 
+}
+?>
+</div>
