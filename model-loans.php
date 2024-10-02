acdeed7c25bf43f8aa68025434ed4e09310546cb
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-02 11:04:18 -0500

    Update model-loans.php

--- model-loans.php
+++ model-loans.php
@@ -2,7 +2,7 @@
 function selectLoans() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT LID, BorrowerID, CheckoutDate, DueDate FROM 'loan'");
+        $stmt = $conn->prepare("SELECT LID, BorrowerID, CheckoutDate, DueDate FROM loan");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
