3d03a96e702043fb688edb81a8642a1f391f11db
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-04 15:44:09 -0500

    Update loans-by-borrower.php

--- loans-by-borrower.php
+++ loans-by-borrower.php
@@ -4,7 +4,7 @@ require_once("model-loans-by-borrower.php");
 
 $pageTitle = "Loans by Borrower";
 include "view-header.php";
-$loansby = selectLoansByBorrower($_GET['id']);
+$loansby = selectLoansByBorrower($_GET['bid']);
 include "view-loans-by-borrower.php";
 include "view-footer.php";
 ?>
