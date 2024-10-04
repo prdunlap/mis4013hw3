4b7faf675471bb79fe72c34ed323faae383359f8
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-04 15:32:49 -0500

    Update and rename copies-by-loan.php to books-by-loan.php

--- books-by-loan.php
+++ books-by-loan.php
@@ -1,10 +1,10 @@
 <?php 
 require_once("utility-db.php");
-require_once("model-copies-by-loan.php");
+require_once("model-books-by-loan.php");
 
 $pageTitle = "Copies by Loan";
 include "view-header.php";
-$copies = selectCopiesByLoan($_GET['cid']);
-include "view-copies-by-loan.php";
+$copies = selectBooksByLoan($_GET['bookid']);
+include "view-books-by-loan.php";
 include "view-footer.php";
 ?>
