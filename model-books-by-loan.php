5e5fcfd4ecd1e88260984adcaf4c0d149052b043
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-04 15:37:37 -0500

    Update model-books-by-loan.php

--- model-books-by-loan.php
+++ model-books-by-loan.php
@@ -2,7 +2,7 @@
 function selectBooksByLoan($lid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT b.BID, l.LID, b.Title FROM book b JOIN loan l ON b.LID = l.LID=?");
+        $stmt = $conn->prepare("SELECT b.BID, l.LID, b.Title FROM book b JOIN loan l ON b.LID = l.LID WHERE b.lid=?");
          $stmt->bind_param("i", $lid);
         $stmt->execute();
         $result = $stmt->get_result();
