d2ac8a209055211a9121df38297f612213878456
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-04 15:34:06 -0500

    Update books-by-loan.php

--- books-by-loan.php
+++ books-by-loan.php
@@ -2,9 +2,9 @@
 require_once("utility-db.php");
 require_once("model-books-by-loan.php");
 
-$pageTitle = "Copies by Loan";
+$pageTitle = "Books by Loan";
 include "view-header.php";
-$copies = selectBooksByLoan($_GET['bookid']);
+$copies = selectBooksByLoan($_POST['lid']);
 include "view-books-by-loan.php";
 include "view-footer.php";
 ?>
