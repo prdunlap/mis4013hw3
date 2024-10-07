5a8b0e6c3ea29f8795a3dcc3b728d550f8474e8d
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 13:08:43 -0500

    Create model-authorinformation.php

---
+++ model-authorinformation.php
@@ -0,0 +1,13 @@
+<?php
+function selectAuthorInformation($aid) {
+
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT author.FName, author.LName, author.Suffix, book.Title, book.PubDate FROM author JOIN bookauthor ON author.AID = bookauthor.AID JOIN book ON bookauthor.BID = book.BID WHERE author.AID=?");
+        $stmt->bind_param("i", $aid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        
+        return $result;
+}
+?>
