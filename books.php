40768b94b9003a391dd017006854f72b7e636f86
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 08:22:20 -0500

    Update books.php

--- books.php
+++ books.php
@@ -4,7 +4,7 @@ require_once("model-books.php");
 
 $pageTitle = "Books";
 include "view-header.php";
-$borrowers = selectBorrowers();
+$borrowers = selectBooks();
 include "view-books.php";
 include "view-footer.php";
 ?>
