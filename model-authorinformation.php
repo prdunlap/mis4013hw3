62f211c563cbf11083bb3808d4540ac4dc385649
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 13:29:05 -0500

    Update model-authorinformation.php

--- model-authorinformation.php
+++ model-authorinformation.php
@@ -2,7 +2,7 @@
 function selectAuthorInfo($aid) {
 
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT author.FName, author.LName, author.Suffix, book.Title, book.PubDate FROM author JOIN bookauthor ON author.AID = bookauthor.AID JOIN book ON bookauthor.BID = book.BID WHERE author.AID=?");
+        $stmt = $conn->prepare("SELECT author.AID, author.FName, author.LName, author.Suffix, book.Title, book.PubDate FROM author JOIN bookauthor ON author.AID = bookauthor.AID JOIN book ON bookauthor.BID = book.BID WHERE author.AID=?");
         $stmt->bind_param("i", $aid);
         $stmt->execute();
         $result = $stmt->get_result();
