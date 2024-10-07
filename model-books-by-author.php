c6af530928e8eed36ae89b0d5ce02b7df3e427fa
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 14:54:30 -0500

    Update model-books-by-author.php

--- model-books-by-author.php
+++ model-books-by-author.php
@@ -1,23 +1,23 @@
 <?php 
   function selectBooks() {
-    try {
+     
         $conn = get_db_connection();
       $stmt = $conn->prepare("SELECT FName, LName, Suffix, Country_of_Origin FROM author");
       $stmt->execute();
       $result=$stmt->get_result();
       $conn->close();
       return $result;
-    } 
+     
       }
 
     function selectAuthorsByBook() {
-      try {
+      
         $conn = get_db_connection();
       $stmt = $conn->prepare("SELECT a.FName, a.LName, a.Suffix, a.Country_of_Origin FROM author a JOIN bookauthor ba ON a.AID = ba.AID JOIN book b on ba.BID = b.BID");
       $stmt->execute();
         $result=$stmt->get_result();
       $conn->close();
       return $result;
-      } 
+       
     }  
 ?>
