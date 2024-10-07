2d5db6f8cd88a73f99e5b0fd0ecdda4c2f941303
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 14:34:51 -0500

    Update view-books-with-authors.php

--- view-books-with-authors.php
+++ view-books-with-authors.php
@@ -7,19 +7,17 @@ while ($book = $books->fetch_assoc()) {
       <h5 class="card-title"><td><?php echo $book['Title']; ?></td></h5>
       <p class="card-text">
   <?php 
-      $bookauthors = selectBooksByAuthor();
+      $bookauthors = selectBooksByAuthor($book['BID']);
+      while ($bookauthor = $bookauthors->fetch_assoc()) {
+        ?>
+        
+      <?php
+      }
   ?>
       </p>
       <p class="card-text"><td>Publication Date: <?php echo $book['PubDate']; ?></td></p>
     </div>
   </div>
-  <tr>
-    <td><?php echo $book['BID']; ?></td>
-    <td><?php echo $book['GID']; ?></td>
-    <td><?php echo $book['Title']; ?></td>
-    <td><?php echo $book['PubDate']; ?></td>
-    <td><?php echo $book['LID']; ?></td>
-  </tr>
 <?php 
 }
 ?>
