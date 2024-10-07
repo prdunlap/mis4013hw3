0545ba30f603082e816e0fdb87fac53c9828d491
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 14:42:11 -0500

    Create books-with-authors.php

---
+++ books-with-authors.php
@@ -0,0 +1,10 @@
+<?php 
+require_once("utility-db.php");
+require_once("model-books-by-author.php");
+
+$pageTitle = "Book Information";
+include "view-header.php";
+$bookauthors = selectBooksByAuthor();
+include "view-books-with-authors.php";
+include "view-footer.php";
+?>
