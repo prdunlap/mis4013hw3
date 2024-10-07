4a2efead353486ef983ec7f4c7c08f33e1a0aa75
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 13:05:44 -0500

    Create author-information.php

---
+++ author-information.php
@@ -0,0 +1,10 @@
+<?php 
+require_once("utility-db.php");
+require_once("model-authorinformation.php");
+
+$pageTitle = "Author Information";
+include "view-header.php";
+$authorsinfo = selectAuthorInfo($_POST['aid']);
+include "view-authorinformation.php";
+include "view-footer.php";
+?>
