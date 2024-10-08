66bbc5d2168b355c4417f3dfd7f41f0cb6ba8643
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-08 14:34:42 -0500

    Update view-books-with-authors.php

--- view-books-with-authors.php
+++ view-books-with-authors.php
@@ -4,22 +4,20 @@ while ($bookauthor = $bookauthors->fetch_assoc()) {
 ?>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title"><td><?php echo $bookauthor['Title']; ?></td></h5>
+      <h5 class="card-title"><td><?php echo $bookauthor['book.Title']; ?></td></h5>
       <p class="card-text">
   <?php 
       while ($bookauthor = $bookauthors->fetch_assoc()) {
         ?>
-        <td><?php echo $bookauthor['AID']; ?></td>
     <td><?php echo $bookauthor['FName']; ?></td>
     <td><?php echo $bookauthor['LName']; ?></td>
     <td><?php echo $bookauthor['Suffix']; ?></td>
-    <td><?php echo $bookauthor['Title']; ?></td>
-    <td><?php echo $bookauthor['PubDate']; ?></td>
+    <td><?php echo $bookauthor['book.PubDate']; ?></td>
       <?php
       }
   ?>
       </p>
-      <p class="card-text"><td>Publication Date: <?php echo $bookauthor['PubDate']; ?></td></p>
+      <p class="card-text"><td>Publication Date: <?php echo $bookauthor['book.PubDate']; ?></td></p>
     </div>
   </div>
 <?php 
