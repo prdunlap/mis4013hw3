1d19aa500e4000087f14e30d6e7ddd8b4c7f5521
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-08 15:18:54 -0500

    Update view-books-with-authors.php

--- view-books-with-authors.php
+++ view-books-with-authors.php
@@ -5,19 +5,19 @@ while ($bookauthor = $bookauthors->fetch_assoc()) {
 ?>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title"><td><?php echo $bookauthor['Title']; ?></td></h5>
+      <h5 class="card-title"><?php echo $bookauthor['Title']; ?></h5>
       <p class="card-text">
    <ul class="list-group">
   <?php 
-      $bookinformation = selectAuthorsByBook($bookauthor['bid']);
-  while ($bookinformation = $bookinfos->fetch_assoc()) {
+      $bookinformation = selectAuthorsByBook($bookauthor['BID']);
+  while ($bookinfos = $bookinformation->fetch_assoc()) {
     ?>
    
-      <li class="list-group-item">Book ID: <?php echo $bookinformation['book.BID']; ?></li>
-      <li class="list-group-item">Genre ID: <?php echo $bookinformation['book.GID']; ?></li>
-      <li class="list-group-item">Author FName: <?php echo $bookinformation['author.FName']; ?></li>
-      <li class="list-group-item">Author LName: <?php echo $bookinformation['author.LName']; ?></li>
-      <li class="list-group-item">Author Country of Origin: <?php echo $bookinformation['author.Country_of_Origin']; ?></li>
+      <li class="list-group-item">Book ID: <?php echo $bookinfos['book.BID']; ?></li>
+      <li class="list-group-item">Genre ID: <?php echo $bookinfos['book.GID']; ?></li>
+      <li class="list-group-item">Author FName: <?php echo $bookinfos['author.FName']; ?></li>
+      <li class="list-group-item">Author LName: <?php echo $bookinfos['author.LName']; ?></li>
+      <li class="list-group-item">Author Country of Origin: <?php echo $bookinfos['author.Country_of_Origin']; ?></li>
 
       <?php  
   }
