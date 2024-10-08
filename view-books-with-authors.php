d7f1088a2289bda91ffb97a2bd2867ed419e7137
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-08 15:14:03 -0500

    Update view-books-with-authors.php

--- view-books-with-authors.php
+++ view-books-with-authors.php
@@ -24,7 +24,7 @@ while ($bookauthor = $bookauthors->fetch_assoc()) {
   ?>
   </ul>
       </p>
-      <p class="card-text"><td>Publication Date: <?php echo $bookauthor['PubDate']; ?></td></p>
+      <p class="card-text">Publication Date: <?php echo $bookauthor['PubDate']; ?></p>
     </div>
   </div>
 <?php 
