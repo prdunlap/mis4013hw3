0a166001e519fe65f2b00a5f39bb4a88d1255f53
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-07 19:06:06 -0500

    Update model-books-by-author.php

--- model-books-by-author.php
+++ model-books-by-author.php
@@ -13,7 +13,7 @@
     function selectAuthorsByBook($aid) {
       
         $conn = get_db_connection();
-      $stmt = $conn->prepare("SELECT a.FName, a.LName, a.Suffix, a.Country_of_Origin FROM author a JOIN bookauthor ba ON a.AID = ba.AID JOIN book b on ba.BID = b.BID");
+      $stmt = $conn->prepare("SELECT a.FName, a.LName, a.Suffix, a.Country_of_Origin FROM author a JOIN bookauthor ba ON a.AID = ba.AID JOIN book b on ba.BID = b.BID WHERE a.AID=?");
       $stmt->bind_param("i",$aid);
       $stmt->execute();
         $result=$stmt->get_result();
