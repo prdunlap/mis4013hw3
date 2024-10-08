ac4f2e5c95637a453fc22185a840ab0f02d0df2e
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-08 15:06:26 -0500

    Update view-books-with-authors.php

--- view-books-with-authors.php
+++ view-books-with-authors.php
@@ -1,4 +1,5 @@
 <h1>Author Books</h1>
+<div class="card-group">
 <?php
 while ($bookauthor = $bookauthors->fetch_assoc()) {
 ?>
@@ -7,19 +8,17 @@ while ($bookauthor = $bookauthors->fetch_assoc()) {
       <h5 class="card-title"><td><?php echo $bookauthor['Title']; ?></td></h5>
       <p class="card-text">
   <?php 
-      while ($bookinformation = $bookauthors->fetch_assoc()) {
-        ?>
-    <td><?php echo $bookinformation['author.FName']; ?></td>
-    <td><?php echo $bookinformation['author.LName']; ?></td>
-    <td><?php echo $bookinformation['author.Suffix']; ?></td>
-    <td><?php echo $bookinformation['book.PubDate']; ?></td>
-      <?php
-      }
+      $bookinformation = selectAuthorsByBook($bookauthor['bid']);
+  while ($bookinformation = $bookinfos->fetch_assoc()) {
+    ?>
+
+      <?php  
+  }
   ?>
       </p>
-      <p class="card-text"><td>Publication Date: <?php echo $bookauthor['book.PubDate']; ?></td></p>
+      <p class="card-text"><td>Publication Date: <?php echo $bookinformation['book.PubDate']; ?></td></p>
     </div>
-
+  </div>
 <?php 
 }
 ?>  
