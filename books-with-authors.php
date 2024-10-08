1984a636aeeb93bc104170da01a634f7473a4c42
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-08 15:06:38 -0500

    Update books-with-authors.php

--- books-with-authors.php
+++ books-with-authors.php
@@ -5,7 +5,6 @@ require_once("model-books-by-author.php");
 $pageTitle = "Book Information";
 include "view-header.php";
 $bookauthors = selectBooks();
-$bookinformation = selectAuthorsByBook($bid);
 include "view-books-with-authors.php";
 include "view-footer.php";
 ?>
