8c00a8526824464941422e3583489ce43b08f509
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-08 15:52:27 -0500

    Update view-books-with-authors.php

--- view-books-with-authors.php
+++ view-books-with-authors.php
@@ -13,12 +13,12 @@ while ($bookauthor = $bookauthors->fetch_assoc()) {
   while ($bookinfos = $bookinformation->fetch_assoc()) {
     ?>
    
-      <li class="list-group-item">Book ID: <?php echo $bookinfos['BID']; ?></li>
-      <li class="list-group-item">Genre ID: <?php echo $bookinfos['GID']; ?></li>
-      <li class="list-group-item">Author FName: <?php echo $bookinfos['FName']; ?></li>
-      <li class="list-group-item">Author LName: <?php echo $bookinfos['LName']; ?></li>
-      <li class="list-group-item">Author Country of Origin: <?php echo $bookinfos['Country_of_Origin']; ?></li>
-      <li class="list-group-item">Publication Date: <?php echo $bookauthor['PubDate']; ?></li>
+      <li class="list-group-item">Book ID - <?php echo $bookinfos['BID']; ?></li>
+      <li class="list-group-item">Genre ID - <?php echo $bookinfos['GID']; ?></li>
+      <li class="list-group-item">Author FName - <?php echo $bookinfos['FName']; ?></li>
+      <li class="list-group-item">Author LName - <?php echo $bookinfos['LName']; ?></li>
+      <li class="list-group-item">Author Country of Origin - <?php echo $bookinfos['Country_of_Origin']; ?></li>
+      <li class="list-group-item">Publication Date - <?php echo $bookauthor['PubDate']; ?></li>
       <?php  
   }
   ?>
