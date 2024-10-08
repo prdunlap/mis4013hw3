f1eee4c4d4444717154b85d58219d05b93e66dd6
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-08 15:13:28 -0500

    Update view-books-with-authors.php

--- view-books-with-authors.php
+++ view-books-with-authors.php
@@ -7,14 +7,22 @@ while ($bookauthor = $bookauthors->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><td><?php echo $bookauthor['Title']; ?></td></h5>
       <p class="card-text">
+   <ul class="list-group">
   <?php 
       $bookinformation = selectAuthorsByBook($bookauthor['bid']);
   while ($bookinformation = $bookinfos->fetch_assoc()) {
     ?>
+   
+      <li class="list-group-item">Book ID: <?php echo $bookinformation['book.BID']; ?></li>
+      <li class="list-group-item">Genre ID: <?php echo $bookinformation['book.GID']; ?></li>
+      <li class="list-group-item">Author FName: <?php echo $bookinformation['author.FName']; ?></li>
+      <li class="list-group-item">Author LName: <?php echo $bookinformation['author.LName']; ?></li>
+      <li class="list-group-item">Author Country of Origin: <?php echo $bookinformation['author.Country_of_Origin']; ?></li>
 
       <?php  
   }
   ?>
+  </ul>
       </p>
       <p class="card-text"><td>Publication Date: <?php echo $bookinformation['book.PubDate']; ?></td></p>
     </div>
