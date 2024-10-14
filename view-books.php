2fe44a7349784011286e2257631e894da0b7b0d1
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-14 13:31:05 -0500

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -1,4 +1,12 @@
-<h1>Books</h1>
+<div class="row">
+  <div class="col"><h1>Books</h1></div>
+  <div class="col-auto">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+    </svg>  
+  </div> 
+</div>
+
 <div class="table-responsive">
   <table class="table">
     <thead> 
