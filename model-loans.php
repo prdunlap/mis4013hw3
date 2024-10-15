43585db11f160da06959263c44319d69109573e3
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-15 16:06:39 -0500

    Update model-loans.php

--- model-loans.php
+++ model-loans.php
@@ -11,5 +11,38 @@ function selectLoans()
 
 }
 
+function insertLoans($lBorrowerID, $lCheckoutDate) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO author (BorrowerID, CheckoutDate) VALUES (?,?)");
+        $stmt->bind_param("id",$lBorrowerID, $lCheckoutDate);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
 
+}
+
+function updateLoans($lBorrowerID, $lCheckoutDate) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE loan SET BorrowerID = ?, CheckoutDate = ? WHERE LID = ?)");
+        $stmt->bind_param("id",$lBorrowerID, $lCheckoutDate);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
+
+}
+function deleteLoans($LID) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE from loan WHERE LID = ?");
+        $stmt->bind_param("i",$LID);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
+
+}
 ?>
