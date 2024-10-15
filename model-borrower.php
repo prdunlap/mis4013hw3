79455a7cb2332751a063b94ae01882388bd5ec9d
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-15 15:57:22 -0500

    Update model-borrower.php

--- model-borrower.php
+++ model-borrower.php
@@ -9,6 +9,39 @@ function selectBorrowers() {
         
         return $result;
 }
+function insertBorrowers($boFName, $boLName, $boDOB, $boemail, $bophone) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO borrower (FName, LName, DOB, email, phone) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("ssdsi",$boFName, $boLName, $boDOB, $boemail, $bophone);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
 
+}
+
+function updateBorrowers($boFName, $boLName, $boDOB, $boemail, $bophone) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE book SET FName = ?, LName = ?, DOB = ?, email = ?, phone = ? WHERE BorrowerID = ?)");
+        $stmt->bind_param("issi",$bGID, $bTitle, $bPubDate, $bLID, $bBID);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
+
+}
+function deleteBorrowers($BorrowerID) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE from book WHERE BorrowerID = ?");
+        $stmt->bind_param("i",$BID);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
+
+}
 
 ?>
