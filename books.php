964ef535217259375385d19db96e782ce8dad6ec
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-15 14:07:18 -0500

    Update books.php

--- books.php
+++ books.php
@@ -4,6 +4,14 @@ require_once("model-books.php");
 
 $pageTitle = "Books";
 include "view-header.php";
+
+if (isset($_POST['actionType')) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertBooks($_POST['GID'], $_POST['bookTitle'], $_POST['pubDate'], $_POST['LID']);
+      break;
+  }
+}
 $books = selectBooks();
 include "view-books.php";
 include "view-footer.php";
