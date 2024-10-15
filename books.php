7941fddff692ee65ae82921220dfb58823d2ba4a
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-15 14:54:40 -0500

    Update books.php

--- books.php
+++ books.php
@@ -12,7 +12,7 @@ if (isset($_POST['actionType'])) {
     echo '<div class="alert alert-success" role="alert">
   Book Added </div>';
 } else {
-  echo '<div class="alert alert-danger" role="alert">Error</div>'
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
     }
       break;
   }
