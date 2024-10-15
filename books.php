863f69a3750142abedba7b5cbbd2d63866d3500c
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-15 14:47:34 -0500

    Update books.php

--- books.php
+++ books.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-    if (insertBooks($_POST['GID'], $_POST['Title'], $_POST['pubDate'], $_POST['LID'])); {
+    if (insertBooks($_POST['GID'], $_POST['Title'], $_POST['pubDate'], $_POST['LID'])) {
     echo '<div class="alert alert-success" role="alert">
   Book Added </div>';
 } else {
