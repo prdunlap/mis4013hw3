f051770a0fdf9f5499d6362c22c9d88a79e792a0
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-15 16:18:23 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -4,6 +4,20 @@ require_once("model-authors.php");
 
 $pageTitle = "Authors";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+    if (insertBooks($_POST['FName'], $_POST['LName'], $_POST['Suffix'], $_POST['DOB'], $_POST['Country_of_Origin'])) {
+    echo '<div class="alert alert-success" role="alert">
+  Book Added </div>';
+} else {
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+      break;
+  }
+}
+
 $authors = selectAuthors();
 include "view-authors.php";
 include "view-footer.php";
