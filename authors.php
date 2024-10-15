ae28e587393a5808e1acaa985fecef846381772b
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-15 16:23:29 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-    if (insertBooks($_POST['FName'], $_POST['LName'], $_POST['Suffix'], $_POST['DOB'], $_POST['Country_of_Origin'])) {
+    if (insertAuthors($_POST['FName'], $_POST['LName'], $_POST['Suffix'], $_POST['DOB'], $_POST['Country_of_Origin'])) {
     echo '<div class="alert alert-success" role="alert">
   Book Added </div>';
 } else {
