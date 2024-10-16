0ea7ecefaa0429bfa1705259b75a14508729abe9
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-16 13:26:13 -0500

    Update author-information.php

--- author-information.php
+++ author-information.php
@@ -4,7 +4,7 @@ require_once("model-authorinformation.php");
 
 $pageTitle = "Author Information";
 include "view-header.php";
-$authorsinfo = selectAuthorInfo($_POST['aid']);
+$authorsinfo = selectAuthorInfo($_POST['AID']);
 include "view-authorinformation.php";
 include "view-footer.php";
 ?>
