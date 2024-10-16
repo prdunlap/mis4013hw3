4cc16b87d3f5080d43bf31026918401c449c2f6f
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-16 13:36:26 -0500

    Update borrower.php

--- borrower.php
+++ borrower.php
@@ -4,6 +4,27 @@ require_once("model-borrower.php");
 
 $pageTitle = "Borrowers";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+    if (insertBorrowers($_POST['FName'], $_POST['LName'], $_POST['DOB'], $_POST['email'], $_POST['phone'])) {
+    echo '<div class="alert alert-success" role="alert">
+  Author Added </div>';
+} else {
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+    case "Delete":
+    if (deleteAuthors($_POST['BorrowerID'])) {
+    echo '<div class="alert alert-success" role="alert">
+  Author Deleted </div>';
+} else {
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+      break;
+  }
+}
+
 $borrowers = selectBorrowers();
 include "view-borrowers.php";
 include "view-footer.php";
