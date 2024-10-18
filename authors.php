dad8deb4d0d9290572a59443d4ef81f8ff8b1ce0
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-18 10:28:48 -0500

    Update authors.php

--- authors.php
+++ authors.php
@@ -14,6 +14,15 @@ if (isset($_POST['actionType'])) {
 } else {
   echo '<div class="alert alert-danger" role="alert">Error</div>';
     }
+    break;
+    case "Edit":
+    if (insertAuthors($_POST['FName'], $_POST['LName'], $_POST['Suffix'], $_POST['DOB'], $_POST['Country_of_Origin'], $_POST['AID'])) {
+    echo '<div class="alert alert-success" role="alert">
+  Author Edited </div>';
+} else {
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+    break;
     case "Delete":
     if (deleteAuthors($_POST['AID'])) {
     echo '<div class="alert alert-success" role="alert">
