59792105040d330e0949cc0724dc1a9b05e45e79
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-18 10:14:52 -0500

    Update view-authors.php

--- view-authors.php
+++ view-authors.php
@@ -43,7 +43,7 @@ while ($author = $authors->fetch_assoc()) {
     <td>
       <form action="" method="POST">
         <input type="hidden" name="AID" value="<?php echo $author['AID']; ?>">
-        <input type="hidden" name="actionType" value="Delete">
+        <input type="hidden" name="actionType" value="Delete" onclick="return confirm('Are you sure?');">
         <button type="submit">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
