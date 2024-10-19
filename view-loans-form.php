abd129e4c8fa0baabbfd2de98e26fe7e0e2eab3e
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-19 17:58:09 -0500

    Update view-loans-form.php

--- view-loans-form.php
+++ view-loans-form.php
@@ -1,15 +1,15 @@
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newborrowerModal">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newloanModal">
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
   </svg>  
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="newborrowerModal" tabindex="-1" aria-labelledby="newborrowerModalLabel" aria-hidden="true">
+<div class="modal fade" id="newloanModal" tabindex="-1" aria-labelledby="newloanModelLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newborrowerModalLabel">New Book</h1>
+        <h1 class="modal-title fs-5" id="newborrowerModalLabel">New Loan</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
