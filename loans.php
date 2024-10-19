6cbf005d55ca2b657a0daf1232c0d1fa5aa88511
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-19 17:53:31 -0500

    Update loans.php

--- loans.php
+++ loans.php
@@ -4,6 +4,37 @@ require_once("model-loans.php");
 
 $pageTitle = "Loans";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+    if (insertLoans($_POST['BorrowerID'], $_POST['CheckoutDate'], $_POST['DueDate'])) {
+    echo '<div class="alert alert-success" role="alert">
+  Loan Added </div>';
+} else {
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+    break;
+    case "Edit":
+    if (updateLoans($_POST['BorrowerID'], $_POST['CheckoutDate'], $_POST['DueDate'],$_POST['LID'])) {
+    echo '<div class="alert alert-success" role="alert">
+  Loan Edited </div>';
+    } else {
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+      break;
+    case "Delete":
+    if (deleteLoans($_POST['LID'])) {
+    echo '<div class="alert alert-success" role="alert">
+  Loan Deleted </div>';
+} else {
+  echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+      break;
+  }
+}
+
+
 $loans = selectLoans();
 include "view-loans.php";
 include "view-footer.php";
