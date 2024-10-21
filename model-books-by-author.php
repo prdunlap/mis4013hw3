89b1f84bada1bc1723c2eae76b32ab0f5dba4fe6
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-10-21 13:22:04 -0500

    Update model-books-by-author.php

--- model-books-by-author.php
+++ model-books-by-author.php
@@ -21,4 +21,39 @@
       return $result;
        
     }  
+
+function insertBooks($bGID, $bTitle, $bPubDate, $bLID) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO book (GID, Title, Pubdate, LID) VALUES (?,?,?,?)");
+        $stmt->bind_param("issi",$bGID, $bTitle, $bPubDate, $bLID);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
+
+}
+
+function updateBooks($bGID, $bTitle, $bPubDate, $bLID, $BID) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE book SET GID = ?, Title = ?, Pubdate = ?, LID = ? WHERE BID = ?");
+        $stmt->bind_param("issii",$bGID, $bTitle, $bPubDate, $bLID, $BID);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
+
+}
+function deleteBooks($BID) 
+     {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE from book WHERE BID = ?");
+        $stmt->bind_param("i",$BID);
+        $success = $stmt->execute();
+        $conn->close();
+        
+        return $success;
+
+}
 ?>
