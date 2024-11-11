2bebe3ad98d8d1ce46eb30562a03b68dd9d26392
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-11-11 13:26:08 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -14,9 +14,9 @@
   </head>
   <body>
     <div class="container">
-      <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+      <nav class="navbar navbar-expand-lg bg-secondary navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Frontier Library System</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
